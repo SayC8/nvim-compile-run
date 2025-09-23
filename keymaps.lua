d95a4594f911117a47c3dfda3b5b0050b4e367de
Adam Robinson <sayc8apex@gmail.com>
2025-09-23 23:42:21 +0100

    Update

--- keymaps.lua
+++ keymaps.lua
@@ -3,41 +3,47 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<F7>", function()
-	local filename = vim.fn.expand("%")
-	local basename = vim.fn.expand("%:r")
-	local filetype = vim.bo.filetype
-	local cmd = nil
-	local function checkFile(f)
-		local c = io.open(f)
-		if c then
-			return true
-		else
-			print("No " .. f .. " found in CWD")
-			f.close()
-			return false
-		end
-	end
-	local function compile(c)
-		vim.cmd("w")
-		vim.cmd("split")
-		vim.cmd("resize 20")
-		vim.cmd(cmd)
-		vim.cmd("$")
-	end
+  local browser = "chromium"
+  local cmd = nil
+  local filetype = vim.bo.filetype
+  local filename = vim.fn.expand("%")
+  local basename = vim.fn.expand("%:r")
 
-	if filetype == "odin" then
-		cmd = "term odin run ."
-		compile(cmd)
-	elseif filetype == "c" then
-		if checkFile("Makefile") then
-			cmd = "term make && ./" .. basename
-			compile(cmd)
-		else
-			print("No Makefile found!")
-		end
-	elseif filetype == "html" then
-		vim.cmd("!firefox %")
-	else
-		print("No interpreter or compiler defined for filetype: '" .. filetype .. "'")
-	end
+  local function checkFile(f)
+    local check = io.open(f)
+    if check then
+      return true
+    else
+      print("No " .. f .. " found in CWD")
+      f.close()
+      return false
+    end
+  end
+
+  local function compile(c)
+    vim.cmd("w")
+    vim.cmd("split")
+    vim.cmd("resize 20")
+    vim.cmd(c)
+    vim.cmd("$")
+  end
+
+  if filetype == "odin" then
+    cmd = "term odin run ."
+    compile(cmd)
+  elseif filetype == "c" then
+    if checkFile("Makefile") then
+      cmd = "term make && ./" .. basename
+      compile(cmd)
+    else
+      print("No Makefile found!")
+    end
+  elseif filetype == "python" then
+    cmd = "term python " .. filename
+    compile(cmd)
+  elseif filetype == "html" then
+    vim.cmd("!" .. browser .. " %")
+  else
+    print("No interpreter or compiler defined for filetype: '" .. filetype .. "'")
+  end
 end)
